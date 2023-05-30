142eb1fd2e33466f8fe48e7199e6a64f24f8cb68
hahuyhoang411 <64120343+hahuyhoang411@users.noreply.github.com>
2023-05-30 11:02:01 +0700

    Update train.py

--- train.py
+++ train.py
@@ -76,7 +76,7 @@ class Trainer:
             self.ctx = nullcontext()
         else:
             # TODO Otherwise, use 'torch.amp.autocast' context with the specified dtype, and initialize GradScaler if mixed_precision_dtype is float16.
-            self.ctx = torch.amp.autocast(device=f"cuda:{self.gpu_id}", dtype=mixed_precision_dtype)
+            self.ctx = torch.amp.autocast(device=f"cuda:{self.gpu_id}", dtype=torch.float16)
             if mixed_precision_dtype == torch.float16:
                 self.gradscaler = GradScaler()
 ######## DONE    
