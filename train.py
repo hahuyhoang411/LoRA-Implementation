819f1f2aa5b71006673988de9bb90d71f4834a7b
hahuyhoang411 <64120343+hahuyhoang411@users.noreply.github.com>
2023-05-30 16:25:50 +0700

    Update train.py

--- train.py
+++ train.py
@@ -359,7 +359,7 @@ if __name__ == "__main__":
         # After that, you should set the 'local_rank' from the environment variable 'LOCAL_RANK'.
         
         # Initialize the process group ### YOUR CODE HERE ###
-        init_process_group(backend='nccl')
+        init_process_group(backend=backend)
         local_rank = int(os.environ['LOCAL_RANK']) ### YOUR CODE HERE ###
     else:
         os.environ['RANK'] = '0'
