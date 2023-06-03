069fe2aa341eb46f0fb929ca404d86d1e2d903d0
hahuyhoang411 <64120343+hahuyhoang411@users.noreply.github.com>
2023-06-03 20:51:35 +0700

    Update train.py

--- train.py
+++ train.py
@@ -334,8 +334,8 @@ if __name__ == "__main__":
     size_valid_set = 0.1
     max_length = 512
     num_epochs = 5
-    batch_size = 2
-    gradient_accumulation_steps = 8
+    batch_size = 4
+    gradient_accumulation_steps = 16
 
     learning_rate = 3e-4
     lr_scheduler_type = 'cosine'
